8f17443199ecf021bff1a93b642a97b470942414
LucyLu23 <114943897+LucyLu23@users.noreply.github.com>
2022-11-21 21:07:18 -0700

    REST API for Goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,16 +1,19 @@
 const asyncHandler = require('express-async-handler')
 
+const Goal = require('../models/goalModel')
+
 // @desc   Get goals
 //@route GET /api/goals
 //@access Private
 const getGoals = asyncHandler( async (req, res) => {
-    res.status(200).json({message: 'Get goals'})
+    const goals = await Goal.find()
+    res.status(200).json(goals)  //{message: 'Get goals'}
 })
 
 // @desc   Set goals
 //@route POST /api/goals
 //@access Private
-const setGoal = asyncHandler( async (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
     //console.log(req.body)
     if(!req.body.text) {
         //res.status(400).json({message:'Please add a text field'})
@@ -18,21 +21,44 @@ const setGoal = asyncHandler( async (req, res) => {
         throw new Error('Please add a text field')
     }
 
-    res.status(200).json({ message: 'Set goal'})
+    const goal = await Goal.create({
+        text:req.body.text,
+    })
+
+    res.status(200).json(goal) //{ message: 'Set goal'}
 })
 
 // @desc    Update goal
 //@route  PUT /api/goals/:id
 //@access Private
 const updateGoal = asyncHandler( async (req, res) => {
-    res.status(200).json({ message: `Update goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal) {
+        res.status(400)
+        throw new Error ('Goal not found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body,{
+        new: true,
+    })
+
+    res.status(200).json(updatedGoal) //{ message: `Update goal ${req.params.id}`}
 })
 
 // @desc   Delete goal
 //@route  DELETE /api/goals
 //@access  Private
 const deleteGoal = asyncHandler( async (req, res) => {
-    res.status(200).json({ message: `Delete goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+   await goal.remove()
+
+    res.status(200).json( {id: req.params.id}) // { message: `Delete goal ${req.params.id}`}
 })
 
 
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 //console.log("Hello World")
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require ('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 6666
+
+connectDB()
 
 const app = express()
 
