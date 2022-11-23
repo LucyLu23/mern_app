365b5a86d03c511688371ddd81f5f4f34d3721e8
LucyLu23 <114943897+LucyLu23@users.noreply.github.com>
2022-11-22 22:51:02 -0700

    finishedTheThirdVideoMernApp

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+//import React from 'react'
 
-import {useState, useEffect} from 'react'
-import {FaUser} from 'react-icons/fa'
+import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { FaUser } from 'react-icons/fa'
+import {register, reset} from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
+
 
 function Register() {
   const [formData, setFormData ] = useState({
@@ -13,9 +19,56 @@ function Register() {
   })
 
   const {name, email, password, password2 } = formData
-  const onChange = () => {}
 
-  return <>
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const {user, isLanding, isError, isSuccess, message} = useSelector
+  (
+    (state) => state.auth
+  )
+
+  useEffect(() => {
+    if(isError){
+      toast.error(message)
+    }
+    if(isSuccess || userData){
+      navigate('/')
+    }
+
+    dispatch(reset())
+
+  }, [user, isError, isSuccess, message, navigate, dispatch])
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]:e.target.value,
+  }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    if(password !== password2) {
+      toast.error('Passwords do not match')
+    } else {
+      const userData = {
+        name,
+        email,
+        password,
+      }
+
+      dispatch (register(userData))
+   }
+  }
+
+  if (isLanding) {
+    return <Spinner />
+  }
+
+  return (
+  <>
     <section className='heading'>
       <h1>
         <FaUser /> Register
@@ -23,8 +76,8 @@ function Register() {
       <p>Please create an account</p>
     </section>
 
-    <section>
-      <form>
+    <section className='form'>
+      <form onSubmit={{onSubmit}}>
         <div className="form-group">
           <input 
           type="text" 
@@ -75,7 +128,8 @@ function Register() {
     </section>
 
     </>
-  
+  )
 }
+
 
 export default Register
